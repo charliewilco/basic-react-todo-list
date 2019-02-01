60ea25ad1776c272b4a1e93e0c4552fb7e241f63
Charles ⚡ <charlespeters42@gmail.com>
2019-01-31 23:52:21 -0500

    adding a title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { TodoItem, ListView, INITIAL_LIST } from "./list-view";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="Container">
-       <ListView initialTasks={INITIAL_LIST} />
+        <h1>Add a Todo</h1>
+        <ListView initialTasks={INITIAL_LIST} />
       </div>
     );
   }
