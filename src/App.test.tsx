553b2fcfca1a605795862c4554276f0a3b5a06a4
Charles ⚡ <charlespeters42@gmail.com>
2019-02-08 13:32:27 -0800

    adding settings

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { ListView, INITIAL_LIST } from "./list-view";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <div className="Container">
+      <h1>Add a Task</h1>
+      <ListView initialTasks={INITIAL_LIST} />
+    </div>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ListView, INITIAL_LIST } from "./list-view";
 import "./index.css";
-import App from "./app";
+
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <div className="Container">
+    <h1>Add a Task</h1>
+    <ListView initialTasks={INITIAL_LIST} />
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
